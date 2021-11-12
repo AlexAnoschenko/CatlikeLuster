6bf93e0be5c96e4341f9429d6bce96d4b0f80fe0
= <=>
2021-11-12 14:48:18 +0300

    change title tab text

--- src/components/ParentsModal/ParentsModal.tsx
+++ src/components/ParentsModal/ParentsModal.tsx
@@ -4,28 +4,28 @@ import ImageZoom from '../ImageZoom/ImageZoom';
 import { ParentsModalProps } from './types';
 
 const ParentsModal = ({
-    isModalVisible,
-    handleCancel,
-    title,
-    currentParent,
+  isModalVisible,
+  handleCancel,
+  title,
+  currentParent,
 }: ParentsModalProps) => {
-    return (
-        <Modal
-            title={title}
-            visible={isModalVisible}
-            onCancel={handleCancel}
-            footer={null}
-            width={1200}
-        >
-            <div className='flex-col'>
-                <div className='pb-10 text-3xl font-bold'>
-                    {currentParent.title}
-                </div>
+  return (
+    <Modal
+      title={title}
+      visible={isModalVisible}
+      onCancel={handleCancel}
+      footer={null}
+      width={1200}
+    >
+      <div className='flex-col'>
+        <div className='pb-10 text-3xl font-bold'>
+          {currentParent.title}
+        </div>
 
-                <ImageZoom images={currentParent.images} />
-            </div>
-        </Modal>
-    );
+        <ImageZoom images={currentParent.images} />
+      </div>
+    </Modal>
+  );
 };
 
 export default ParentsModal;
--- src/pages/Parents/Parents.tsx
+++ src/pages/Parents/Parents.tsx
@@ -8,45 +8,42 @@ import { parentsCards } from './constants';
 import { ParentProps } from './types';
 
 const Parents: FC = () => {
-    const [isModalVisible, setIsModalVisible] = useState(false);
-    const [currentParent, setCurrentParent] = useState<ParentProps>({
-        images: [],
-        title: '',
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [currentParent, setCurrentParent] = useState<ParentProps>({
+    images: [],
+    title: '',
+  });
+
+  const showModal = (parent: ParentProps) => {
+    setIsModalVisible(true);
+    setCurrentParent(parent);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+    setCurrentParent({
+      images: [],
+      title: '',
     });
+  };
 
-    const showModal = (parent: ParentProps) => {
-        setIsModalVisible(true);
-        setCurrentParent(parent);
-    };
+  return (
+    <div className='flex justify-center min-h-content page px-50px pt-60'>
+      <ParentsModal
+        isModalVisible={isModalVisible}
+        handleCancel={handleCancel}
+        currentParent={currentParent}
+      />
 
-    const handleCancel = () => {
-        setIsModalVisible(false);
-        setCurrentParent({
-            images: [],
-            title: '',
-        });
-    };
-
-    return (
-        <div className='flex justify-center min-h-content page px-50px pt-60'>
-            <ParentsModal
-                isModalVisible={isModalVisible}
-                handleCancel={handleCancel}
-                currentParent={currentParent}
-            />
-
-            <Row gutter={[70, 40]}>
-                {parentsCards.map((parent) => (
-                    <Col key={parent.title}>
-                        <ParentCard
-                            parent={parent}
-                            showModal={showModal}
-                        />
-                    </Col>
-                ))}
-            </Row>
-        </div>
-    );
+      <Row gutter={[70, 40]}>
+        {parentsCards.map((parent) => (
+          <Col key={parent.title}>
+            <ParentCard parent={parent} showModal={showModal} />
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
 };
 
 export default Parents;
