ce0b5fe60b9b7f072a90c51d8d09afaba27f58e8
= <=>
2021-11-22 15:52:21 +0300

    add viber, whatsApp and phone numbers

--- src/pages/Contacts/constants.ts
+++ src/pages/Contacts/constants.ts
@@ -1,24 +1,44 @@
 import { ContactsProps } from './types';
 
 export const contacts: ContactsProps[] = [
-    {
-        name: 'Facebook',
-        icon: 'https://i.ibb.co/PjXc56Q/facebook.png',
-        link: 'https://www.facebook.com/groups/615481596558858/',
-    },
-    {
-        name: 'TikTok',
-        icon: 'https://i.ibb.co/mcqPrJ6/tiktok.png',
-        link: 'https://vm.tiktok.com/ZM8XD6UtA/',
-    },
-    {
-        name: 'Instagram',
-        icon: 'https://i.ibb.co/r4ccFkF/instagram.png',
-        link: 'https://instagram.com/catlike.luster.bengals?utm_medium=copy_link',
-    },
-    {
-        name: 'catlike.luster@gmail.com',
-        icon: 'https://i.ibb.co/C26xnvK/gmail.png',
-        link: 'mailto:catlike.luster@gmail.com',
-    },
+  {
+    name: 'Facebook',
+    icon: 'https://i.ibb.co/PjXc56Q/facebook.png',
+    link: 'https://www.facebook.com/groups/615481596558858/',
+  },
+  {
+    name: 'TikTok',
+    icon: 'https://i.ibb.co/mcqPrJ6/tiktok.png',
+    link: 'https://vm.tiktok.com/ZM8XD6UtA/',
+  },
+  {
+    name: 'Instagram',
+    icon: 'https://i.ibb.co/r4ccFkF/instagram.png',
+    link: 'https://instagram.com/catlike.luster.bengals?utm_medium=copy_link',
+  },
+  {
+    name: 'Viber',
+    icon: 'https://firebasestorage.googleapis.com/v0/b/catlikeluster-b7e7d.appspot.com/o/Contact%20Page%2Fviber.png?alt=media&token=55108d46-a158-481d-98f4-ec60ffa79a53',
+    link: 'https://msng.link/o/?375296469970=vi',
+  },
+  {
+    name: 'WhatsApp',
+    icon: 'https://firebasestorage.googleapis.com/v0/b/catlikeluster-b7e7d.appspot.com/o/Contact%20Page%2FWhatsApp.png?alt=media&token=0c2237c7-1813-4756-9cb8-8503547f19aa',
+    link: 'https://msng.link/o/?375296469970=wa',
+  },
+  {
+    name: 'catlike.luster@gmail.com',
+    icon: 'https://i.ibb.co/C26xnvK/gmail.png',
+    link: 'mailto:catlike.luster@gmail.com',
+  },
+  {
+    name: '+375296469970',
+    icon: 'https://firebasestorage.googleapis.com/v0/b/catlikeluster-b7e7d.appspot.com/o/Contact%20Page%2Fmobile.png?alt=media&token=2c0d66e9-a9ea-4089-8e3d-838d376610cb',
+    link: 'tel:+375296469970',
+  },
+  {
+    name: '+375259911592',
+    icon: 'https://firebasestorage.googleapis.com/v0/b/catlikeluster-b7e7d.appspot.com/o/Contact%20Page%2Fmobile.png?alt=media&token=2c0d66e9-a9ea-4089-8e3d-838d376610cb',
+    link: 'tel:+375259911592',
+  },
 ];
