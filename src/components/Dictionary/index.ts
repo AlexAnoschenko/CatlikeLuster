d9df2b5d52db70c60a961d27a9411f02ff125cdc
= <=>
2021-11-22 11:53:02 +0300

    dictionary extension

--- src/components/Dictionary/index.ts
+++ src/components/Dictionary/index.ts
@@ -1,37 +1,49 @@
 import { IDictionary } from './types';
 
 const setLanguage = (lang: string): IDictionary => {
-    switch (lang) {
-        case 'en':
-            return {
-                main: 'MAIN',
-                parents: 'PARENTS',
-                kittens: 'KITTENS',
-                gallery: 'GALLERY',
-                info: 'INFO',
-                contacts: 'CONTACTS',
-            };
+  switch (lang) {
+    case 'en':
+      return {
+        main: 'MAIN',
+        parents: 'PARENTS',
+        kittens: 'KITTENS',
+        gallery: 'GALLERY',
+        info: 'INFO',
+        contacts: 'CONTACTS',
+        contactsText:
+          'Delivery of kittens is possible anywhere in the world, please specify the delivery price and any other questions in the way.',
+        kittensForSale: 'FOR SALE',
+        kittensSold: 'SOLD',
+      };
 
-        case 'ru':
-            return {
-                main: 'ГЛАВНАЯ',
-                parents: 'РОДИТЕЛИ',
-                kittens: 'КОТЯТА',
-                gallery: 'ГАЛЕРЕЯ',
-                info: 'ИНФО',
-                contacts: 'КОНТАКТЫ',
-            };
+    case 'ru':
+      return {
+        main: 'ГЛАВНАЯ',
+        parents: 'РОДИТЕЛИ',
+        kittens: 'КОТЯТА',
+        gallery: 'ГАЛЕРЕЯ',
+        info: 'ИНФО',
+        contacts: 'КОНТАКТЫ',
+        contactsText:
+          'Доставка котят возможна в любую точку мира, пожалуйста, уточняйте стоимость доставки и любые другие вопросы.',
+        kittensForSale: 'НА ПРОДАЖУ',
+        kittensSold: 'ПРОДАНЫ',
+      };
 
-        default:
-            return {
-                main: 'MAIN',
-                parents: 'PARENTS',
-                kittens: 'KITTENS',
-                gallery: 'GALLERY',
-                info: 'INFO',
-                contacts: 'CONTACTS',
-            };
-    }
+    default:
+      return {
+        main: 'MAIN',
+        parents: 'PARENTS',
+        kittens: 'KITTENS',
+        gallery: 'GALLERY',
+        info: 'INFO',
+        contacts: 'CONTACTS',
+        contactsText:
+          'Delivery of kittens is possible anywhere in the world, please specify the delivery price and any other questions in the way.',
+        kittensForSale: 'FOR SALE',
+        kittensSold: 'SOLD',
+      };
+  }
 };
 
 export default setLanguage;
--- src/pages/Contacts/Contacts.tsx
+++ src/pages/Contacts/Contacts.tsx
@@ -1,34 +1,42 @@
-import { FC } from "react";
+import { FC } from 'react';
 
-import { contacts } from "./constants";
+import setLanguage from '../../components/Dictionary';
+import { useTypedSelector } from '../../hooks/useTypedSelector';
+import { contacts } from './constants';
 
 const Contacts: FC = () => {
-  return (
-    <div className="bg-bg-pages page pt-50px px-50px">
-      <div className="mb-6 text-5xl font-bold text-center">Contacts</div>
+  const { language } = useTypedSelector((state) => state.language);
+  const dict = setLanguage(language);
 
-      <div className="mb-20 text-2xl text-center">
-        Delivery of kittens is possible anywhere in the world, please specify
-        the delivery price and any other questions in the way.
+  return (
+    <div className='bg-bg-pages page pt-50px px-50px'>
+      <div className='mb-6 text-5xl font-bold text-center'>
+        {dict.contacts}
       </div>
 
-      <div className="flex justify-center">
-        <div className="w-min">
+      <div className='mb-20 text-2xl text-center'>
+        {dict.contactsText}
+      </div>
+
+      <div className='flex justify-center'>
+        <div className='w-min'>
           {contacts.map((contact) => (
             <a
               href={contact.link}
-              target="_blank"
-              rel="noreferrer"
+              target='_blank'
+              rel='noreferrer'
               key={contact.name}
             >
-              <div className="flex items-center mb-6">
+              <div className='flex items-center mb-6'>
                 <img
                   src={contact.icon}
-                  alt="logo"
-                  className="mr-4 w-contact-icon"
+                  alt='logo'
+                  className='mr-4 w-contact-icon'
                 />
 
-                <div className="text-2xl font-bold">{contact.name}</div>
+                <div className='text-2xl font-bold'>
+                  {contact.name}
+                </div>
               </div>
             </a>
           ))}
