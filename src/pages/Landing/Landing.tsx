52a554cfaf61c49ea96aaa0dc6d8184b69140b08
Aliaksei <aliaksei.anoshchanka@aventri.com>
2021-11-23 12:45:10 +0300

    fix logo size

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -14,7 +14,7 @@ const Landing: FC = () => {
         <img
           src='https://firebasestorage.googleapis.com/v0/b/catlikeluster-b7e7d.appspot.com/o/Landing%20Page%2Fmain-logo.png?alt=media&token=900e0e8a-af49-4681-84e2-df71a9f820f2'
           alt='main-logo'
-          width={isTabletOrMobile ? 220 : 400}
+          width={isTabletOrMobile ? 300 : 400}
         />
       </div>
     </div>
